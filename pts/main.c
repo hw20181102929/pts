e7967fe7d0ba0aed9bcb44bf859e1cd94f88f47a
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-17 15:48:04 +0800

    Initial Commit

---
+++ pts/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pts
+//
+//  Created by s20181102929 on 2019/9/17.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
